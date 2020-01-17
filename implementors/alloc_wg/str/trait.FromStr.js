5e56bb4943e2c4d801393fa73272601757f4a158
TimDiekmann <TimDiekmann@users.noreply.github.com>
2020-01-17 12:37:09 +0000

    Automated deployment: Fri Jan 17 12:37:09 UTC 2020 da4637a49a4e4b64e7f0785f16220fedb4cdf80a

--- implementors/alloc_wg/str/trait.FromStr.js
+++ implementors/alloc_wg/str/trait.FromStr.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["alloc_wg"] = [{text:"impl <a class=\"trait\" href=\"alloc_wg/str/trait.FromStr.html\" title=\"trait alloc_wg::str::FromStr\">FromStr</a> for <a class=\"struct\" href=\"alloc_wg/string/struct.String.html\" title=\"struct alloc_wg::string::String\">String</a>",synthetic:false,types:["alloc_wg::string::String"]},];
+implementors["alloc_wg"] = [{"text":"impl <a class=\"trait\" href=\"alloc_wg/str/trait.FromStr.html\" title=\"trait alloc_wg::str::FromStr\">FromStr</a> for <a class=\"struct\" href=\"alloc_wg/string/struct.String.html\" title=\"struct alloc_wg::string::String\">String</a>","synthetic":false,"types":["alloc_wg::string::String"]}];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
