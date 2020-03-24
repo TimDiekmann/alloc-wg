95695007a41bd11381055d138b6a571a6508d5d9
TimDiekmann <TimDiekmann@users.noreply.github.com>
2020-03-24 21:30:58 +0000

    Automated deployment: Tue Mar 24 21:30:58 UTC 2020 46df75465eb624ce87b09ced9cd81d1f08d0b57b

--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["alloc_wg"] = {"name":"","dirs":[{"name":"alloc","files":["layout.rs","mod.rs"]},{"name":"str","files":["mod.rs"]}],"files":["boxed.rs","clone.rs","collections.rs","iter.rs","lib.rs","ptr.rs","raw_vec.rs","string.rs","vec.rs"]};
+sourcesIndex["alloc_wg"] = {"name":"","dirs":[{"name":"str","files":["mod.rs"]}],"files":["alloc.rs","boxed.rs","clone.rs","collections.rs","iter.rs","lib.rs","ptr.rs","raw_vec.rs","string.rs","vec.rs"]};
 createSourceSidebar();
