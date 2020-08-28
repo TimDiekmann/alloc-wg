307ad784582473cbcbf34ddd117c605815929ca3
TimDiekmann <TimDiekmann@users.noreply.github.com>
2020-08-28 01:29:50 +0000

    Automated deployment: Fri Aug 28 01:29:50 UTC 2020 9ae078b3e4f89259a1ad1f973b79750acb30c9db

---
+++ implementors/core/ops/bit/trait.BitAnd.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["alloc_wg"] = [{"text":"impl&lt;T:&nbsp;Ord + Clone, A:&nbsp;AllocRef + Clone, '_, '_&gt; BitAnd&lt;&amp;'_ BTreeSet&lt;T, A&gt;&gt; for &amp;'_ BTreeSet&lt;T, A&gt;","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
